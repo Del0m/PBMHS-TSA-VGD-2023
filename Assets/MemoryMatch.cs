fab0f2c8fe44ba4f9f7084b19067954f054f11c0
Del0m <98620378+Del0m@users.noreply.github.com>
2022-11-26 20:58:59 -0500

    memorymatch doesn't show cards on startup, properly appears and disappears values

--- Assets/MemoryMatch.cs
+++ Assets/MemoryMatch.cs
@@ -15,25 +15,24 @@ public class MemoryMatch : GameHandler
     {
         base.JumpStart();
         //build cards
-        BuildCards();
+        BuildCards(10); // 10 cards to be built
     }
-    private void BuildCards()
+    private void BuildCards(int cardAmount) // grabs given cards, and gives them random numbers
     {
         Debug.Log("building cards");
         //randomizes card data and spawns it on the cards.
         var cards = CollectCards();
 
-        var sort = new int[10];
+        var sort = new int[cardAmount];
         //randomizer formula for card picks
         for (int i = 0; i < sort.Length - 1; i++) // to be put in a function L8R
         {
-            var randInt = Random.Range(0, 10); // includes value above since max is exclusive for Random.Range
-            Debug.Log("Sorting!");
+            var randInt = Random.Range(0, cardAmount); // includes value above since max is exclusive for Random.Range
             for (int j = 0; j < i; j++) // for loop to check for repeat values
             {
                 while (randInt == sort[j]) // got same value? reroll
                 {
-                    randInt = Random.Range(0, 10);
+                    randInt = Random.Range(0, cardAmount);
                 }
             }
             sort[i] = randInt;
@@ -43,12 +42,15 @@ public class MemoryMatch : GameHandler
         //putting numbers on the cards
         for(int i = 0; i < cards.Length - 1; i+=2)
         {
-
             //grabs two from sort; assigns a number to them, rinse and repeat
-            Debug.Log("Assigning value " + num + " to cards:" + i + " and " + (i + 1));
             //ran twice to pick two cards that will be modified
             cards[sort[i]].GetComponentInChildren<TextMeshProUGUI>().text = num.ToString(); // turns int into string.
             cards[sort[i+1]].GetComponentInChildren<TextMeshProUGUI>().text = num.ToString(); //i+1 to grab the next randomized spot
+
+            //sets all card number colors to be faded so player doesn't know what cards are what
+            cards[i].GetComponentInChildren<TextMeshProUGUI>().CrossFadeAlpha(0, 0, false);
+            cards[i+1].GetComponentInChildren<TextMeshProUGUI>().CrossFadeAlpha(0, 0, false);
+
 
             num++; //make new number to put on card
         }
@@ -78,21 +80,32 @@ public class MemoryMatch : GameHandler
 
         return cardUI;
     }
-    string[] cardValue = new string[2] { null, null };
+    string[] cardValue = new string[2] { null, null }; // vars for OnPress
     public void OnPress(GameObject ui) // button flip over 
     {
-        Debug.Log("Pressing " + ui);
         Debug.Log(clicked);
         cardsPicked[clicked] = ui;
 
         cardValue[clicked] = ui.GetComponentInChildren<TextMeshProUGUI>().text; // add value of card onto array to check if cards are the same l8r
         clicked++;
-        ui.GetComponentInChildren<TextMeshProUGUI>().color = new Color(0, 0, 0); // set to black
+        //ui.GetComponentInChildren<TextMeshProUGUI>().color = new Color(0, 0, 0); // set to black
+        ui.GetComponentInChildren<TextMeshProUGUI>().CrossFadeAlpha(255, 0.2f, false);
+
+
+        
 
         if (clicked >= 2) // see if two cards are flipped
         {
+            //using wait coroutine to show both cards before disappearing
+            StartCoroutine(ButtonRoutine(2)); // coroutine wait function and holder of rest of script cuz i don't know how to fix it.... 
+        }
+
+        IEnumerator ButtonRoutine(int time) // local IEnumerator since it won't be needed anywhere else.
+        {
+            yield return new WaitForSeconds(time);
+
             //two cards are flipped, compare cardValue[] values together, true; score, false; don't score
-            if(cardValue[0] == cardValue[1] && cardsPicked[0] != cardsPicked[1])
+            if (cardValue[0] == cardValue[1] && cardsPicked[0] != cardsPicked[1])
             {
                 //give 1 point
                 score[gameOrder]++;
@@ -110,9 +123,8 @@ public class MemoryMatch : GameHandler
 
             for (int i = 0; i < cards.Length; i++) // for loop to change all cards
             {
-                cards[i].GetComponentInChildren<TextMeshProUGUI>().color = new Color(255, 255, 255);
+                cards[i].GetComponentInChildren<TextMeshProUGUI>().CrossFadeAlpha(0, 0, false);
             }
         }
-
     }
 }
