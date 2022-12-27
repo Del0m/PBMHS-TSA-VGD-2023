a1dbdedf3e2f8e15672a76c94a39c4858f08f119
Armin Delmo <armindelmo00@gmail.com>
2022-12-27 01:10:38 -0500

    adding top-down controls and making jigsaw minigame test scene

---
+++ Assets/JigsawPuzzle.cs
@@ -0,0 +1,21 @@
+// armin delmo; JigsawPuzzle.cs
+// the purpose of this program is to construct a jigsaw puzzle game where players grab the pieces to their puzzle
+// to construct it before everyone else.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JigsawPuzzle : GameHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
