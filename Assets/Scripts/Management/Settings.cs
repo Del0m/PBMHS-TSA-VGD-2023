1e278ae24d26dd7fef548d2a3f1ea4de99cacc7d
Armin Delmo <armindelmo00@gmail.com>
2023-01-20 14:03:50 -0500

    housekeeping; improving jigsaw puzzle game

--- Assets/Scripts/Management/Settings.cs
+++ Assets/Scripts/Management/Settings.cs
@@ -10,9 +10,18 @@ public class Settings : MonoBehaviour
     public float soundVolume;
     public float musicVolume;
     public float masterVolume;
+    int fps; 
     private void Start()
     {
         LoadSettings(); // load the player's settings for volume
+        SetFrameRate(fps); // set frame rate for end-user
+    }
+    public void SetFrameRate(int frame) // sets target frame rate for user
+    {
+        PlayerPrefs.SetInt("fps", frame);
+        Application.targetFrameRate = fps;
+
+        LoadSettings();
     }
     public void ModifyVolume(string setting, float amount) // for the purposes of altering volume for the player
     {
@@ -24,5 +33,8 @@ public class Settings : MonoBehaviour
         soundVolume = PlayerPrefs.GetFloat("soundVolume", 1f);
         musicVolume = PlayerPrefs.GetFloat("musicVolume", 1f);
         masterVolume = PlayerPrefs.GetFloat("masterVolume", 1f);
+        fps = PlayerPrefs.GetInt("fps", 60);
+
+        PlayerPrefs.Save(); // save all settings done
     }
 }
