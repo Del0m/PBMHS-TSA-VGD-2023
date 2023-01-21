b86f37ddf634a3dcac642881a217f6f94ae4fd2f
Armin Delmo <armindelmo00@gmail.com>
2023-01-21 09:48:50 -0500

    added knight and wizard pictures; added them in imagescript

--- Assets/Scripts/Handler/MiniGames/JigsawPuzzle/ImageScript.cs
+++ Assets/Scripts/Handler/MiniGames/JigsawPuzzle/ImageScript.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class ImageScript : MonoBehaviour
 {
-    public Sprite[] coconutSplice; // coconut.jpg
+    public Sprite[] knightIcon; // playable knight character in game; detail
+    public Sprite[] wizardIcon; // playable wizard character in game; detail
     public Sprite[] ImageSelection() // returns the texture2d array that is selected via switch
     {
-        var randomNum = Random.Range(0, 1); // 0 - 1 atm b/c of one image only atm
+        var randomNum = Random.Range(0, 2); // 0 - 1 atm b/c of one image only atm
 
         switch (randomNum) // switch with all random imagery.
         {
-            case 0: // coconut.jpg
-                return coconutSplice;
+            case 0:
+                return knightIcon;
+            case 1:
+                return wizardIcon;
+
             default:
                 return null;
         }
