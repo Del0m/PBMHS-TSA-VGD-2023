c189daa26a632fe8efd2bab5c6844b59925acde0
Armin Delmo <armindelmo00@gmail.com>
2023-01-23 22:41:03 -0500

    updating ImageScript to properly not allow repeats, worked on etc, too lazy to write the rest

--- Assets/Scripts/Handler/MiniGames/JigsawPuzzle/ImageScript.cs
+++ Assets/Scripts/Handler/MiniGames/JigsawPuzzle/ImageScript.cs
@@ -6,9 +6,10 @@ public class ImageScript : MonoBehaviour
 {
     public Sprite[] knightIcon; // playable knight character in game; detail
     public Sprite[] wizardIcon; // playable wizard character in game; detail
+
+    public int[] pictureChosen; // to prevent multiple runs
     public Sprite[] ImageSelection() // returns the texture2d array that is selected via switch
     {
-        var randomNum = Random.Range(0, 2); // 0 - 1 atm b/c of one image only atm
 
         switch (randomNum) // switch with all random imagery.
         {
@@ -20,5 +21,20 @@ public class ImageScript : MonoBehaviour
             default:
                 return null;
         }
+    }
+    public int ChangeRepeat() // change repeat drawings of the images
+    {
+        var randNum = Random.Range(0, 2); // 0 - 1 atm b/c of one image only atm
+
+        for (int i = 0; i < pictureChosen.Length; i++) // for loop to change repeats
+        {
+            if(randNum == pictureChosen[i]) { randNum = Random.Range(0, 2); }
+        }
+        for (int i = 0; i < pictureChosen.Length; i++)
+        {
+            if (randNum == pictureChosen[i]) { ChangeRepeat(); break; } // recurse function until no repeat
+        }
+
+        return randNum;
     }
 }
