4bbc7e6f8eff7873a4e2d7485a0b9d311d1651ac
Armin Delmo <armindelmo00@gmail.com>
2023-01-25 00:28:09 -0500

    color platforms in color match now drop accordingly

--- Assets/ColorMatch.cs
+++ Assets/ColorMatch.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class ColorMatch : GameHandler
 {
-    // Start is called before the first frame update
+    [Header("Colors")] // order in arrays is: blue, red, green, yellow
+    public string[] colorString =
+    {
+        "blue",
+        "red",
+        "green",
+        "yellow"
+    };
+    public GameObject[] colorFlag; // color flag to show
+    public GameObject[] colorPlatform; // colored platforms that'll be dropped
+
+    [Header("Game Settings")]
+    public float dropTime; // time till they drop
+    public int dropAmount; // amount the platforms will drop
+
+
     void Start()
     {
-        
+        StartCoroutine(StartGame(true)); // starting game and bringing players into the game
+        StartCoroutine(DropColors()); 
+    }
+    int ChooseColor()
+    {
+        // randomly select color
+        return Random.Range(0, colorString.Length);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator DropColors() // drop colors that ain't the chosen one
     {
-        
+        for(int i = 0; i < dropAmount; i++)
+        {
+            var colorChosen = ChooseColor(); // color they will have to go onto
+            // show color to player
+            Debug.Log(colorString[colorChosen]); // show in testing 
+
+            yield return new WaitForSeconds(dropTime); // time they have left till drop
+            
+            // for loop to drop all those platforms that ain't it
+            for(int j = 0; j < colorString.Length; j++)
+            {
+                if(colorChosen != j)
+                {
+                    var colorScript = colorPlatform[j].GetComponent<ColorPlatform>();
+                    colorScript.dropPlatform = true;
+                }
+            }
+            yield return new WaitForSeconds(2); // wait for them do DIEE >:}
+            for(int k = 0; k < colorString.Length; k++)
+            {
+                var colorScript = colorPlatform[k].GetComponent<ColorPlatform>();
+                colorScript.dropPlatform = false;
+            }
+            dropTime -= (dropTime / 10); // reduce drop time to up the stakes
+        }
     }
+
 }
---
+++ Assets/ColorPlatform.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPlatform : MonoBehaviour
+{
+    private Vector2 startingPos; // this position is going to move back when it drops
+    [Header("Drop Location")]
+    public Transform dropPos; // position to drop to
+
+    public bool dropPlatform; // to be switched in ColorMatch minigame
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlatform();
+    }
+    void MovePlatform()
+    {
+        if(dropPlatform)
+        {
+            Debug.Log("Moving down!");
+            transform.position = Vector2.MoveTowards(this.transform.position, dropPos.position, 10 * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, startingPos, 10 * Time.deltaTime);
+
+        }
+    }
+}
