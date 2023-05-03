4ae17d95fc920dcfe067d31738a075e0b29d8416
Armin Delmo <armindelmo00@gmail.com>
2023-05-03 09:27:50 -0400

    moving player animations to "PlayerAnimation" script

---
+++ Assets/PlayerAnimation.cs
@@ -0,0 +1,19 @@
+// armin delmo; PlayerAnimation.cs; this will control the player's animations.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
